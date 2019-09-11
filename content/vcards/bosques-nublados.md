caffe71f395f7d6c3c188fdc4015a5b41d4fe60b
Ricardo Morin <morinricardo@gmail.com>
2019-09-11 12:35:47 -0700

    Design vcard and add some content

--- content/vcards/bosques-nublados.md
+++
@@ -1,5 +0,0 @@
----
-title: "Bosques nublados"
-description: "here is the description 2"
----
-some content 2
--- src/templates/VegetationCard.vue
+++ src/templates/VegetationCard.vue
@@ -8,8 +8,58 @@
           </div>
         </div>
       </section>
+      <figure class="image is-4by3 is-fullwidth">
+        <img :src="$page.vegetationCard.cardimage">
+      </figure>
+      <figcaption class="has-text-centered"><small>{{$page.vegetationCard.cardimagecaption}}</small></figcaption>
       <section class="section">
-        <h1>{{$page.vegetationCard.description}}</h1>
+        <div class="tile is-ancestor">
+          <div class="tile is-vertical is-parent">
+            <div class="is-size-4 is-uppercase has-text-weight-semibold has-text-centered">Descripción</div>
+            <div class="tile is-parent">
+              <div class="tile is-child box">
+                <p v-html="$page.vegetationCard.description" />
+              </div>
+            </div>
+            <div class="is-size-4 is-uppercase has-text-weight-semibold has-text-centered">Distribución</div>
+            <div class="tile is-parent">
+              <div class="tile is-child is-5 box">
+                <p v-html="$page.vegetationCard.distribution" />
+              </div>
+              <div class="tile is-child is-7 box">
+                <figure class="image is-4by3">
+                  <img src="https://bulma.io/images/placeholders/640x480.png">
+                </figure>
+              </div>
+            </div>
+            <div class="is-size-4 is-uppercase has-text-weight-semibold has-text-centered">Cambios en la distribución</div>
+            <div class="tile is-parent">
+              <div class="tile is-child is-6 box">
+                <figure class="image is-4by3">
+                  <img src="https://bulma.io/images/placeholders/640x480.png">
+                </figure>
+              </div>
+              <div class="tile is-child is-6 box">
+                <figure class="image is-4by3">
+                  <img src="https://bulma.io/images/placeholders/640x480.png">
+                </figure>
+              </div>
+            </div>
+            <div class="is-size-4 is-uppercase has-text-weight-semibold has-text-centered">Situación a 2010</div>
+            <div class="tile is-parent">
+              <div class="tile is-child is-6 box">
+                <figure class="image is-4by3">
+                  <img src="https://bulma.io/images/placeholders/640x480.png">
+                </figure>
+              </div>
+              <div class="tile is-child is-6 box">
+                <figure class="image is-4by3">
+                  <img src="https://bulma.io/images/placeholders/640x480.png">
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   </Layout>
@@ -20,6 +70,9 @@
     vegetationCard: vegetationCard (path: $path) {
       title
       description
+      cardimage
+      cardimagecaption
+      distribution
     }
   }
 </page-query>
