c6f75763b160e12a0af5cd74957e91eafd1181c6
Ricardo Morin <morinricardo@gmail.com>
2019-09-11 15:12:35 -0700

    Site name update

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -76,18 +76,6 @@
 export default {
   data () {
     return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ],
       showNav: false
     }
   }
