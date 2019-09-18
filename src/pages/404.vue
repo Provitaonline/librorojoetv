e77a0559ea319ec7f5527dff75458ef441c44d32
Ricardo Morin <morinricardo@gmail.com>
2019-09-17 17:27:40 -0700

    Add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <div class="page-wrapper">
+      <section class="hero is-small is-white">
+        <div class="hero-body">
+          <div class="container is-fullhd has-text-centered">
+            <h1 class="title">NO ENCUENTRO LA PÁGINA (404)...</h1>
+          </div>
+        </div>
+        <div class="section has-text-centered">
+          <img src="/images/404.jpg" alt="logo" >
+        </div>
+      </section>
+    </div>
+  </Layout>
+</template>
