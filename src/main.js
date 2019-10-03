6186f9d723b2790b115ba5625a878517d282953d
Ricardo Morin <morinricardo@gmail.com>
2019-10-03 15:24:59 -0700

    Remove redundant code

--- src/main.js
+++ src/main.js
@@ -13,9 +13,10 @@ import VueScrollTo from 'vue-scrollto'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
 import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faArrowDown, faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false;
-library.add(faFacebook, faTwitter)
+library.add(faFacebook, faTwitter, faArrowDown, faAngleDoubleDown)
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
