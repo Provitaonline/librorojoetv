0fa8c8dfda697bbf8ca502199af0e41fbf02201d
Ricardo Morin <morinricardo@gmail.com>
2019-10-11 10:46:10 -0700

    Et al. italics

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -173,6 +173,7 @@
         isLoading: true,
         zoom: 6,
         url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}",
+        //url: "https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}",
         tileLayerOptions: {
           attribution: 'Tiles © Esri — Source: <a href="https://www.arcgis.com/home/item.html?id=30e5fe3149c34df1ba922e6f5bbf808f">ArcGIS World Topographic Map</a>'
         },
