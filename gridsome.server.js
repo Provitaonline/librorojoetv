3bc32784ef288f7cf2482818c82d9e9c631c44fe
Ricardo Morin <morinricardo@gmail.com>
2019-10-25 11:55:29 -0700

    Add processed images

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,6 +5,8 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const fs = require('fs-extra')
+
 module.exports = function (api) {
   api.loadSource(({ addContentType }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api
@@ -12,5 +14,14 @@ module.exports = function (api) {
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api
+  })
+
+  api.afterBuild(() => {
+    console.log('After build... copy to: ', api.config.imagesDir)
+    fs.copySync(api.config.imagesDir, './pimages')
+  })
+  api.beforeBuild(() => {
+    console.log('Before build... move to: ', api.config.imageCacheDir)
+    fs.moveSync('./pimages', api.config.imageCacheDir, { overwrite: true })
   })
 }
