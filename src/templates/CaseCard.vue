29bad96c87f1a9a0b6afcb5d2a2d19c296a401bd
Ricardo Morin <morinricardo@gmail.com>
2019-11-27 13:23:06 -0800

    Case card formatting

--- src/templates/CaseCard.vue
+++ src/templates/CaseCard.vue
@@ -4,11 +4,53 @@
       <section class="hero is-small is-white">
         <div class="hero-body">
           <div class="container is-fullhd has-text-centered">
-            <h1 class="title">CASO DE ESTUDIO</h1>
+            <g-link to="/cases" class="is-uppercase is-size-6">Casos de estudio</g-link>
           </div>
         </div>
-        <div class="section">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem...
+      </section>
+      <div>
+        <g-image v-if="$page.caseCard.cardimage" :src="$page.caseCard.cardimage" fit="inside" />
+      </div>
+      <figcaption style="padding-left: 12px;"><div class="is-size-6 is-size-7-mobile" v-html="$page.caseCard.cardimagecaption"></div></figcaption>
+      <div class="columns" style="margin-bottom: -48px;">
+        <div class="column section">
+          <p class="title is-uppercase is-size-4-mobile">{{$page.caseCard.title}}</p>
+          <p>Carlos Portillo-Quintero, Pablo Lacabana, Fabián Carrasquel</p>
+        </div>
+        <div class="column is-two-fifths">
+          <div class="card summary-box">
+            <header class="card-header">
+              <p class="card-header-title">
+                WHATEVER
+              </p>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section class="section">
+        <div class="tile is-vertical is-parent">
+          <TextWithRefsAndPhotos class="tile is-child box is-size-5 is-size-6-mobile"
+            :text="$page.caseCard.content"
+            :isContent="true" />
+          </TextWithRefsAndPhotos>
         </div>
       </section>
     </div>
@@ -17,9 +59,37 @@
 
 <page-query>
   query CaseCard ($path: String!) {
-    unitCard: unitCard (path: $path) {
+    caseCard: caseCard (path: $path) {
       title
       content
+      cardimage
+      cardimagecaption
     }
   }
 </page-query>
+
+<style lang="scss" scoped>
+  .card-header-title {
+    background-color: #f8e7e8;
+  }
+
+  @media only screen and (min-width: 769px) {
+    .summary-box {
+      margin-top: -10rem;
+    }
+  }
+</style>
+
+<script>
+  import TextWithRefsAndPhotos from '~/components/TextWithRefsAndPhotos.vue'
+
+  export default {
+
+    mounted () {
+    },
+    components: {
+      TextWithRefsAndPhotos
+    }
+  }
+
+</script>
