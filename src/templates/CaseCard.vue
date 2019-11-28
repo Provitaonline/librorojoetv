be7e26a8a2151eb2f2767b022c19b2bb2ad43668
Ricardo Morin <morinricardo@gmail.com>
2019-11-27 18:40:05 -0800

    Case card layout

--- src/templates/CaseCard.vue
+++ src/templates/CaseCard.vue
@@ -12,35 +12,47 @@
         <g-image v-if="$page.caseCard.cardimage" :src="$page.caseCard.cardimage" fit="inside" />
       </div>
       <figcaption style="padding-left: 12px;"><div class="is-size-6 is-size-7-mobile" v-html="$page.caseCard.cardimagecaption"></div></figcaption>
-      <div class="columns" style="margin-bottom: -48px;">
-        <div class="column section">
+      <div class="columns section is-size-6 is-size-7-mobile" style="margin-bottom: -96px;">
+        <div class="column card-title">
           <p class="title is-uppercase is-size-4-mobile">{{$page.caseCard.title}}</p>
-          <p>Carlos Portillo-Quintero, Pablo Lacabana, Fabián Carrasquel</p>
+          <p>{{$page.caseCard.authors}}</p>
         </div>
-        <div class="column is-two-fifths">
-          <div class="card summary-box">
+        <div class="column is-narrow">
+          <div class="card summary-box" style="box-shadow: none; border-style: solid;">
             <header class="card-header">
               <p class="card-header-title">
-                WHATEVER
+                Paisaje vegetal:&nbsp;<span v-html="$page.caseCard.landscapeunits"></span>
               </p>
             </header>
             <div class="card-content">
               <div class="content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris.
+                <b>Localidad:</b> {{$page.caseCard.locality}}<br>
+                <b>Estados:</b> {{$page.caseCard.states}}<br>
+                <b>Área aprox.:</b> {{$page.caseCard.area | number}} km<sup>2</sup><br>
               </div>
+              <table align="center" class="table is-size-6 is-size-7-mobile">
+                <thead>
+                  <tr style="background-color: #f8e7e8;">
+                    <th v-for="value in $page.caseCard.cardtablecolumns" style="border: none;">{{value}}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="value in $page.caseCard.cardtablerows">
+                    <td v-for="item in value.split('|')">
+                      <div v-if="threatCategoryIcons[item]" style="text-align: center;">
+                        <b-tooltip :label="threatCategories[item].text" position="is-top" type="is-warning">
+                          <div class="iconInTable" style="display: inline-block">
+                            <img :src="threatCategoryIcons[item]">
+                          </div>
+                        </b-tooltip>
+                      </div>
+                      <div v-else>
+                        {{item}}
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
@@ -61,30 +73,70 @@
   query CaseCard ($path: String!) {
     caseCard: caseCard (path: $path) {
       title
+      authors
       content
       cardimage
       cardimagecaption
+      landscapeunits
+      locality
+      states
+      area
+      cardtablecolumns
+      cardtablerows
     }
   }
 </page-query>
 
 <style lang="scss" scoped>
+  @import "~/assets/style/_variables";
+
+  .iconInTable {
+    width: 30px;
+    height: 30px;
+  }
+
   .card-header-title {
+    padding-left: 24px;
+    padding-rigth: 24px;
     background-color: #f8e7e8;
   }
 
   @media only screen and (min-width: 769px) {
     .summary-box {
-      margin-top: -10rem;
+      margin-top: -12rem;
     }
+  }
+
+  @media only screen and (max-width: 768px) {
+    .card-title {
+      text-align: center;
+    }
+  }
+
+  .summary-box {
+    box-shadow: none;
+    border: solid $primary 2px;
   }
 </style>
 
 <script>
+  import {threatCategories} from '~/assets/js/siteConfig.js'
   import TextWithRefsAndPhotos from '~/components/TextWithRefsAndPhotos.vue'
 
-  export default {
+  let threatCategoryIcons = {}
+  for (let key in threatCategories) {
+    threatCategoryIcons[key] = require('~/assets/svgs/' + key + '-icon.svg')
+  }
 
+  console.log(threatCategoryIcons)
+
+  export default {
+    data() {
+      return {
+        threatCategories: threatCategories,
+        threatCategoryIcons: threatCategoryIcons
+      }
+    },
     mounted () {
     },
     components: {
