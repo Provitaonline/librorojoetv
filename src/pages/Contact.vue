53200804d94e836fd812faf5e910aeb8b1aed770
Ricardo Morin <morinricardo@gmail.com>
2019-12-02 15:38:40 -0800

    Tweak hero formats

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -3,7 +3,7 @@
     <div class="page-wrapper">
       <section class="hero is-small is-white">
         <div class="hero-body">
-          <div class="container is-fullhd has-text-centered">
+          <div class="is-uppercase has-text-centered">
             <h1 class="title">CONTACTO</h1>
           </div>
         </div>
