d0ce7b59b821e41fd8f60fd75722bca5827b0eca
Ricardo Morin <morinricardo@gmail.com>
2019-12-06 11:10:02 -0800

    Add side bar

--- src/templates/CaseCard.vue
+++ src/templates/CaseCard.vue
@@ -1,83 +1,108 @@
 <template>
   <Layout>
-    <div class="page-wrapper">
-      <section class="hero is-small is-white">
-        <div class="hero-body">
-          <div class="container is-fullhd has-text-centered">
-            <g-link to="/cases" class="is-uppercase is-size-6">Casos de estudio</g-link>
-          </div>
-        </div>
-      </section>
-      <div>
-        <g-image v-if="$page.caseCard.cardimage" :src="$page.caseCard.cardimage" fit="inside" />
-      </div>
-      <figcaption style="padding-left: 12px;"><div class="is-size-6 is-size-7-mobile" v-html="$page.caseCard.cardimagecaption"></div></figcaption>
-      <div class="columns section is-size-6 is-size-7-mobile" style="margin-bottom: -96px;">
-        <div class="column card-title">
-          <p class="title is-uppercase is-size-4-mobile">{{$page.caseCard.title}}</p>
-          <p>{{$page.caseCard.authors}}</p>
-        </div>
-        <div class="column is-narrow">
-          <div class="card summary-box" style="box-shadow: none; border-style: solid;">
-            <header class="card-header">
-              <p class="card-header-title">
-                Paisaje vegetal:&nbsp;<span v-html="$page.caseCard.landscapeunits"></span>
-              </p>
-            </header>
-            <div class="card-content">
-              <div class="content">
-                <b>Localidad:</b>&nbsp;<span v-html="$page.caseCard.locality"></span><br>
-                <b>Estados:</b> {{$page.caseCard.states}}<br>
-                <b>Área aprox.:</b>
-                  <span v-if="$page.caseCard.areatext" v-html="$page.caseCard.areatext"><br></span>
-                  <span v-else>{{$page.caseCard.area | number}} km<sup>2</sup><br></span>
-              </div>
-              <div :class='$page.caseCard.enableTableScroll ? "table-container" : ""'>
-                <table align="center" class="table is-size-6 is-size-7-mobile">
-                  <thead>
-                    <tr>
-                      <th v-for="value in $page.caseCard.cardtablecolumns" style="border: none;">{{value}}</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr v-for="value in $page.caseCard.cardtablerows">
-                      <td v-for="item in value.split('|')">
-                        <div v-if="xtractedIcon(item)" style="display: flex;">
-                          <div :style='$page.caseCard.leftJustifyIcons ? "" : "margin: auto;"'>
-                            <b-tooltip :label="xtractedIcon(item).text" position="is-top" type="is-warning">
-                              <div class="iconInTable" style="display: inline-block">
-                                <img :src="xtractedIcon(item).img">
-                              </div>
-                            </b-tooltip>
-                          </div>
-                          <div style="margin-top: auto; margin-bottom: auto;">
-                            <span>&nbsp;{{iconText(item)}}</span>
-                          </div>
-                        </div>
-                        <div v-else v-html="item"></div>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+
+    <SideBar>
+      <template v-slot:title>
+        <p class="is-size-4 has-text-weight-bold has-text-centered" style="padding: 20px;">Casos de estudio</p>
+      </template>
+      <template v-slot:sidebar>
+        <div class="box">
+          <div v-for="item, index in $page.casesIndex.legendItems">
+            <div>
+              <div class="side-panel-item-box side-panel-item">
+                <g-link v-if="item.cardPath != $route.path.replace(/\/$/, '').match(/\/[^\/]+$/)[0].substring(1)" :to="'/cases/' + item.cardPath">{{item.label}}</g-link>
+                <span v-else><b><i>{{item.label}}</i></b></span>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <section class="section">
-        <div class="tile is-vertical is-parent">
-          <TextWithRefsAndPhotos style="padding-bottom: 0px; margin-bottom: 0px !important;" class="tile is-child box is-size-5 is-size-6-mobile"
-            :text="$page.caseCard.content"
-            :refs="$page.caseCard.case.references"
-            :isContent="true">
-          </TextWithRefsAndPhotos>
-          <div style="padding-top: 0px;" class="tile is-child box is-size-6 is-size-7-mobile">
-            <h1 style="margin-top: 0px;">Bibliografía</h1>
-            <b-table :data="sortedReferences" :columns="columns" :mobile-cards="false"></b-table>
+      </template>
+
+      <template v-slot:content>
+        <section class="hero is-small is-white">
+          <div class="hero-body">
+            <div class="container is-fullhd has-text-centered">
+              <g-link to="/cases" class="is-uppercase is-size-6">Casos de estudio</g-link>
+            </div>
+          </div>
+        </section>
+        <div>
+          <g-image v-if="$page.caseCard.cardimage" :src="$page.caseCard.cardimage" fit="inside" />
+        </div>
+        <figcaption style="padding-left: 12px;"><div class="is-size-6 is-size-7-mobile" v-html="$page.caseCard.cardimagecaption"></div></figcaption>
+        <div class="columns section is-size-6 is-size-7-mobile" style="margin-bottom: -96px;">
+          <div class="column card-title">
+            <p class="title is-uppercase is-size-4-mobile">{{$page.caseCard.title}}</p>
+            <p>{{$page.caseCard.authors}}</p>
+          </div>
+          <div class="column is-narrow">
+            <div class="card summary-box" style="box-shadow: none; border-style: solid;">
+              <header class="card-header">
+                <p class="card-header-title">
+                  Paisaje vegetal:&nbsp;<span v-html="$page.caseCard.landscapeunits"></span>
+                </p>
+              </header>
+              <div class="card-content">
+                <div class="content">
+                  <b>Localidad:</b>&nbsp;<span v-html="$page.caseCard.locality"></span><br>
+                  <b>Estados:</b> {{$page.caseCard.states}}<br>
+                  <b>Área aprox.:</b>
+                    <span v-if="$page.caseCard.areatext" v-html="$page.caseCard.areatext"><br></span>
+                    <span v-else>{{$page.caseCard.area | number}} km<sup>2</sup><br></span>
+                </div>
+                <div :class='$page.caseCard.enableTableScroll ? "table-container" : ""'>
+                  <table align="center" class="table is-size-6 is-size-7-mobile">
+                    <thead>
+                      <tr>
+                        <th v-for="value in $page.caseCard.cardtablecolumns" style="border: none;">{{value}}</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="value in $page.caseCard.cardtablerows">
+                        <td v-for="item in value.split('|')">
+                          <div v-if="xtractedIcon(item)" style="display: flex;">
+                            <div :style='$page.caseCard.leftJustifyIcons ? "" : "margin: auto;"'>
+                              <b-tooltip :label="xtractedIcon(item).text" position="is-top" type="is-warning">
+                                <div class="iconInTable" style="display: inline-block">
+                                  <img :src="xtractedIcon(item).img">
+                                </div>
+                              </b-tooltip>
+                            </div>
+                            <div style="margin-top: auto; margin-bottom: auto;">
+                              <span>&nbsp;{{iconText(item)}}</span>
+                            </div>
+                          </div>
+                          <div v-else v-html="item"></div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </section>
-    </div>
+        <section class="section">
+          <div class="tile is-vertical is-parent">
+            <TextWithRefsAndPhotos style="padding-bottom: 0px; margin-bottom: 0px !important;" class="tile is-child box is-size-5 is-size-6-mobile"
+              :text="$page.caseCard.content"
+              :refs="$page.caseCard.case.references"
+              :isContent="true">
+            </TextWithRefsAndPhotos>
+            <div style="padding-top: 0px;" class="tile is-child box is-size-6 is-size-7-mobile">
+              <h1 style="margin-top: 0px;">Bibliografía</h1>
+              <b-table :data="sortedReferences" :columns="columns" :mobile-cards="false"></b-table>
+            </div>
+          </div>
+        </section>
+
+
+
+
+      </template>
+
+
+    </SideBar>
   </Layout>
 </template>
 
@@ -105,11 +130,36 @@
         }
       }
     }
+    casesIndex: casesIndex (path: "/content/cases-index") {
+      title
+      mapTitle
+      legendItems {
+        name
+        label
+        group
+        color
+        legend
+        cardPath
+      }
+    }
   }
 </page-query>
 
 <style lang="scss" scoped>
   @import "~/assets/style/_variables";
+
+  .side-panel-item, .side-panel-item-title {
+    padding: 8px;
+  }
+
+  .side-panel-item:hover {
+    background-color: #fafafa;
+  }
+
+  .side-panel-item-box {
+    display: flex;
+    box-sizing: border-box;
+  }
 
   .iconInTable {
     width: 30px;
@@ -153,6 +203,7 @@
 <script>
   import {threatCategories} from '~/assets/js/siteConfig.js'
   import TextWithRefsAndPhotos from '~/components/TextWithRefsAndPhotos.vue'
+  import SideBar from '~/components/SideBar.vue'
 
   export default {
     data() {
@@ -176,7 +227,8 @@
     mounted () {
     },
     components: {
-      TextWithRefsAndPhotos
+      TextWithRefsAndPhotos,
+      SideBar
     },
     computed: {
       sortedReferences: function() {
