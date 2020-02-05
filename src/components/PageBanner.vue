d7e37b687c2811d66e5f558976eb74c43e1e9258
Ricardo Morin <morinricardo@gmail.com>
2020-02-05 15:47:19 -0800

    Adjust PageBanner styles

--- src/components/PageBanner.vue
+++ src/components/PageBanner.vue
@@ -3,7 +3,8 @@
     <section class="hero">
       <g-image class="hero-bg-img" :src="banner" />
       <div class="hero-body">
-        <g-link :to="link" class="hero-link is-size-4 is-size-5-mobile">{{lead}}</g-link>
+        <g-link v-if="link" :to="link" class="hero-link is-size-4 is-size-5-mobile">{{lead}}</g-link>
+        <span class="is-size-4 is-size-5-mobile" v-else v-html="lead"></span>
         <p class="hero-text is-size-2 is-uppercase has-text-weight-bold is-size-4-mobile" v-html="title"></p>
         <p v-if="subtitle" class="hero-text is-size-4 is-size-6-mobile">{{subtitle}}</p>
         <slot name="follow"></slot>
@@ -28,6 +29,7 @@
     position: absolute;
     bottom: 0;
     padding: 10px;
+    padding-left: 40px;
     background: rgba(0, 0, 0, .2);
   }
 
@@ -59,7 +61,7 @@
       banner: { type: Object, required: true },
       caption: { type: String, required: true },
       lead: { type: String, required: true },
-      link: { type: String, required: true },
+      link: { type: String, required: false },
       title: { type: String, required: true },
       subtitle: {type: String, required: false}
     }
