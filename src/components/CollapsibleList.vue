8529d01c9bd6ab09db9f95b4aec4f1526761a89b
Ricardo Morin <morinricardo@gmail.com>
2020-02-08 16:48:32 -0800

    Highlight current item in view

--- src/components/CollapsibleList.vue
+++ src/components/CollapsibleList.vue
@@ -3,17 +3,26 @@
     <div v-for="(parent, index) in list" :key="index">
       <b-collapse :open="false">
         <div style="display: flex; justify-content: space-between;" slot="trigger" slot-scope="props">
-          <g-link v-if="parent.parentLink" :to="parent.parentLink">{{parent.parentLabel}}</g-link>
+          <g-link v-bind:class="{current: ($route.path === parent.parentLink)}" v-if="parent.parentLink" :to="parent.parentLink">{{parent.parentLabel}}</g-link>
           <div v-else>{{parent.parentLabel}}</div>
           <div v-if="parent.children"><font-awesome size="lg" :icon="['fas', !props.open ? 'angle-down' : 'angle-up']" /></div>
         </div>
         <div style="margin-left: 16px;" v-for="(child, index) in parent.children" :key="index">
-          <g-link :to="child.childLink">{{child.childLabel}}</g-link>
+          <g-link v-bind:class="{current: ($route.path === child.childLink)}" :to="child.childLink">{{child.childLabel}}</g-link>
         </div>
       </b-collapse>
     </div>
+    {{$route.path}}
   </div>
 </template>
+
+<style lang="scss" scoped>
+
+  .current {
+    font-style: italic;
+  }
+
+</style>
 
 <script>
 
