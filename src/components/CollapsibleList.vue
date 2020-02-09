eac2b46307c031d18055f7c4610d39f408b61d09
Ricardo Morin <morinricardo@gmail.com>
2020-02-08 16:04:08 -0800

    Add CollapsibleList component

---
+++ src/components/CollapsibleList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="(parent, index) in list" :key="index">
+      <b-collapse :open="false">
+        <div style="display: flex; justify-content: space-between;" slot="trigger" slot-scope="props">
+          <g-link v-if="parent.parentLink" :to="parent.parentLink">{{parent.parentLabel}}</g-link>
+          <div v-else>{{parent.parentLabel}}</div>
+          <div v-if="parent.children"><font-awesome size="lg" :icon="['fas', !props.open ? 'angle-down' : 'angle-up']" /></div>
+        </div>
+        <div style="margin-left: 16px;" v-for="(child, index) in parent.children" :key="index">
+          <g-link :to="child.childLink">{{child.childLabel}}</g-link>
+        </div>
+      </b-collapse>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'CollapsibleList',
+    props: {
+      list: {type: Array, required: true}
+    }
+  }
+
+</script>
