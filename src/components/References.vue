b7f4e32378d7a686a015386f9c90e0c777b82f73
Ricardo Morin <morinricardo@gmail.com>
2020-02-13 12:31:31 -0800

    References component

---
+++ src/components/References.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="is-size-5 has-text-centered">
+      <div class="ref-header">
+        <h1 class="ref-header-text">Bibliografía</h1>
+        <a @click="showMore = !showMore">
+          <span v-if="!showMore"><font-awesome size="lg" :icon="['fas', 'angle-down']"/></span>
+          <span v-else><font-awesome size="lg" :icon="['fas', 'angle-up']"/></span>
+          <br>
+        </a>
+      </div>
+    </div>
+    <transition name="fade" appear>
+      <div v-if="showMore" style="padding-top: 0px;" class="tile is-child box is-size-6 is-size-7-mobile">
+        <table class="table">
+          <tbody>
+            <tr v-for="value in sortedReferences">
+              <td>{{value.reference}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import "~/assets/style/_variables";
+
+  .ref-header {
+    display: flex;
+    align-items: baseline;
+    justify-content: space-between;
+    border-bottom: 4px solid $primary;
+    margin-bottom: 10px;
+  }
+
+  .ref-header-text {
+    padding-bottom: 0;
+    margin-bottom: 20px;
+  }
+
+  tr:nth-child(odd) {
+    background-color: #F8F8F8;
+  }
+
+  td {
+    padding-top: 20px;
+    padding-bottom: 20px;
+    border: none;
+  }
+</style>
+
+
+<script>
+  export default {
+    name: 'References',
+    props: {
+      references: { type: Array, required: true }
+    },
+    data() {
+      return {
+        showMore: false
+      }
+    },
+    computed: {
+      sortedReferences: function() {
+        return this.references.sort((a, b) => a.reference.localeCompare(b.reference))
+      }
+    }
+  }
+</script>
