f5018f17cc7515494a32fc997e1696933a42b8af
Ricardo Morin <morinricardo@gmail.com>
2020-02-17 20:13:37 -0800

    Update contact page

--- src/pages/Contacto.vue
+++ src/pages/Contacto.vue
@@ -1,29 +1,73 @@
 <template>
   <Layout>
     <div class="red-line">
-      <section class="hero is-small is-white">
+      <section class="hero">
+        <g-image class="hero-bg-img" :src="$page.homeData.contact.banner"/>
         <div class="hero-body">
-          <div class="is-uppercase has-text-centered red-line">
-            <h1 class="title">CONTACTO</h1>
-          </div>
+          <g-image src="~/assets/images/provita.png"/>
         </div>
-        <div class="box">
-          <g-image style="display: block; margin: auto;" src="~/assets/images/provita.png"></g-image>
-          <div class="is-size-6 has-text-centered has-text-weight-semibold">
-            <br><br><font-awesome size="2x" :icon="['fas', 'map-marker-alt']"/>
-            <p v-html="$page.homeData.contact.address"></p>
-            <br><font-awesome size="2x" :icon="['fas', 'phone']"/>
-            <p>{{$page.homeData.contact.phone}}</p><br>
-            <br><font-awesome size="2x" :icon="['fas', 'envelope']"/>
-            <p><a :href="'mailto:' + $page.homeData.contact.email">{{$page.homeData.contact.email}}</a></p><br>
-            <br><font-awesome size="2x" :icon="['fas', 'globe-americas']"/>
-            <p><a :href="$page.homeData.contact.web">{{$page.homeData.contact.web}}</a></p>
+      </section>
+
+      <section class="section is-center-narrow">
+        <div class="tile is-ancestor">
+          <div class="tile ">
+            <div class="tile is-parent" style="flex-direction: column;">
+              <font-awesome size="2x" :icon="['fas', 'map-marker-alt']"/>
+              <h1>Provita</h1>
+              <p v-html="$page.homeData.contact.address"></p>
+            </div>
+            <div class="tile is-parent" style="flex-direction: column;">
+              <font-awesome size="2x" :icon="['fas', 'phone']"/>
+              <h1>Teléfonos</h1>
+              <p v-html="$page.homeData.contact.phone"></p><br>
+           </div>
+            <div class="tile is-parent" style="flex-direction: column;">
+              <font-awesome size="2x" :icon="['fas', 'envelope']"/>
+              <h1>Mail</h1>
+              <p><a :href="'mailto:' + $page.homeData.contact.email">{{$page.homeData.contact.email}}</a></p><br>
+            </div>
+            <div class="tile is-parent" style="flex-direction: column;">
+              <font-awesome size="2x" :icon="['fas', 'globe-americas']"/>
+              <h1>Web</h1>
+              <p><a :href="$page.homeData.contact.web">{{$page.homeData.contact.web}}</a></p>
+            </div>
           </div>
         </div>
       </section>
     </div>
   </Layout>
 </template>
+
+<style lang="scss" scoped>
+
+  @import "~/assets/style/_variables";
+  .hero {
+    background-size: cover !important;
+    background-position: center;
+    text-align: center;
+    overflow: hidden;
+    position: relative;
+  }
+
+  .hero-body {
+    position: absolute;
+    width: 225px;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 10px;
+  }
+
+  .hero-bg-img {
+    height: 300px;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  h1 {
+    padding: 0;
+    margin: 0;
+  }
+</style>
 
 <page-query>
   query Home {
@@ -33,6 +77,7 @@
         phone
         email
         web
+        banner
       }
     }
   }
