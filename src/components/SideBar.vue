08c4487fc92cdf260bf00777a62537fca39241f0
Ricardo Morin <morinricardo@gmail.com>
2020-02-19 17:33:44 -0800

    Tidy up

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,9 +3,6 @@
     <aside class="side-panel column is-narrow" v-bind:class="{ isopen: sidePanelOpen }">
       <div class="side-panel-content" v-bind:class="{ isopen: sidePanelOpen }">
         <div class="side-panel-title">
-          <!-- <a title="Cerrar panel" style="float: right; padding-right: 8px;" v-on:click="toggleSidePanelState()">
-            <span class="close-side-panel-icon is-size-5">✕</span>
-          </a> -->
           <a v-if="sidePanelOpen" v-on:click="toggleSidePanelState()" role="button">
             <div title="Cerrar panel lateral" class="side-panel-triangle-left">
               <div class="side-panel-times"><font-awesome :icon="['fas', 'times']"/></div>
@@ -24,14 +21,6 @@
           <div title="Abrir panel lateral" class="side-panel-triangle-right">
             <div class="side-panel-bars"><font-awesome :icon="['fas', 'bars']"/></div>
           </div>
-          <!-- <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 278.571 275.714" class="open-side-panel-icon">
-            <g>
-              <title>Clic para abrir panel de fichas</title>
-              <path  stroke-width="10" stroke-linecap="round" stroke-linejoin="round" d="M10 10h258.57v255.714H10z"></path>
-              <path d="M77.143 10.714L78.57 265" fill-rule="evenodd" stroke-width="10"></path>
-              <path d="M582.857 733.79L210.906 945.68l2.474-428.064z" fill-opacity="0" transform="matrix(.33171 0 0 .39595 48.089 -151.84)" stroke-width="50" stroke-linecap="round" stroke-linejoin="round"></path>
-            </g>
-          </svg> -->
         </a>
       </div>
       <slot name="content"></slot>
@@ -41,10 +30,6 @@
 
 <style lang="scss" scoped>
   @import "~/assets/style/_variables";
-
-  .side-panel {
-    /*border-right: 1px solid #e0e0e0; */
-  }
 
   @media only screen and (min-width: 769px) {
     .side-panel {
@@ -58,7 +43,6 @@
     .side-panel {
       min-width: 20rem;
       border-right: 0;
-      /*border-bottom: 1px solid #e0e0e0; */
     }
   }
 
@@ -77,7 +61,6 @@
   .side-panel-open-button {
     z-index: 1; /* This button must stay on top of content */
     position: absolute;
-    /* padding-top: 10px; */
   }
 
   @media only screen and (min-width: 769px) {
@@ -88,7 +71,6 @@
   }
 
   .side-panel-title {
-    /* background-color: #fafafa; */
     color: $primary;
   }
 
