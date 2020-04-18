d21d0e86f19390ed1a02b7f1bcb948f56f3d9987
Ricardo Morin <morinricardo@gmail.com>
2020-04-18 16:30:00 -0700

    Link style

--- src/components/RiskEvalNote.vue
+++ src/components/RiskEvalNote.vue
@@ -4,10 +4,22 @@
   </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+  @import "~/assets/style/_variables";
+
   .risk-eval-note {
     margin-bottom: 0;
   }
+
+  .risk-eval-note a {
+    color: $link !important;
+  }
+
+  .risk-eval-note a:hover {
+    color: $link-hover !important;
+  }
+
 </style>
 
 <static-query>
